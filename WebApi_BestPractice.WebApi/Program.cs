e52bc4fa25eb15043b8268cc7526a755939c059e
i_Ghaemi <Qaemi66@gmail.com>
2020-07-05 20:07:38 +0430

    define and config logging system

--- WebApi_BestPractice.WebApi/Program.cs
+++ WebApi_BestPractice.WebApi/Program.cs
@@ -1,17 +1,42 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using WebApi_BestPractice.WebApi;
 
-namespace WebApi_BestPractice.WebApi
+namespace MyApi
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //Set deafult proxy
+            //WebRequest.DefaultWebProxy = new WebProxy("http://127.0.0.1:8118", true) { UseDefaultCredentials = true };
+
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging(options => options.ClearProviders())
+                .UseNLog()
                 .UseStartup<Startup>();
     }
 }
