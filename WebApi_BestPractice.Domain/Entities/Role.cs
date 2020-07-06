9f7ded14c83256e26d9e983dd0a9417b99b94d81
i_Ghaemi <Qaemi66@gmail.com>
2020-07-06 17:30:23 +0430

    cleaning code

--- WebApi_BestPractice.Domain/Entities/Role.cs
+++ WebApi_BestPractice.Domain/Entities/Role.cs
@@ -6,12 +6,8 @@ namespace WebApi_BestPractice.Domain.Etities
 {
     public class Role : BaseClasses.BaseEntity {
 
-        [Required]
-        [StringLength(50)]
         public string Name { get; set; }
 
-        [Required]
-        [StringLength(100)]
         public string Description { get; set; }
     }
 
