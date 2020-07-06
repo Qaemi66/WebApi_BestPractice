e1c986e0c7366318be1b06d277f9c80c356c9a06
iman ghaemi <Iman_Ghaemi@hotmail.com>
2020-07-05 22:23:37 -0700

    fix bugs

--- WebApi_BestPractice.WebApi/Program.cs
+++ WebApi_BestPractice.WebApi/Program.cs
@@ -17,7 +17,7 @@ namespace MyApi
             var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
             try
             {
-                logger.Debug("init main");
+                logger.Info("init main");
                 CreateWebHostBuilder(args).Build().Run();
             }
             catch (Exception ex)
