c4215133be372db65fc0be4841a17f8b2ab63879
iman ghaemi <Iman_Ghaemi@hotmail.com>
2020-11-24 15:50:30 +0330

    fix some bugs

---
+++ WebApi_BestPractice.WebApi/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_BestPractice.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class TestController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
